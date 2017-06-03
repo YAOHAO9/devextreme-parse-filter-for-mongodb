f9c0de9aba33bd0630ef606f8a0c91c1a36780cd
yaohao9 <986403268@qq.com>
2017-06-03 13:47:14 +0800

    write test demo and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # devextreme-parse-filter-for-mongodb
 Parse DevExtreme dx-data-grid filter for mongodb
+
+## install
+npm install devextreme-parse-filter-for-mongodb --save
+
+## use
+1. let parseFilter = require('devextreme-parse-filter-for-mongodb')
+
+2. let aggregate = parseFilter(filter)
+
+3. db.yourmodel.aggregate(aggregate).exec().then(models=>{...})
+
+## test
+npm test
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  * Created by YAOHAO on 5/30/2016
  */
 
-let getAggregatefromFilter = require('./index')
+let parseFilter = require('./index')
 
 
 let testDemo = [
@@ -11,6 +11,7 @@ let testDemo = [
     '[[["supplier.scalaId","=","100103"],"or",["supplier.scalaId","=","300005"],"or",["supplier.scalaId","=","900641"]],"and",["supplier.name","=","Roquette GmbH"]]',
     '[[["supplier.name","=","Sallandse Golfclub de Hoek"],"or",["supplier.name","=","Witsel, A.R."]],"and",[[["dateUpdated",">=","2017-04-24T16:00:00.000Z"],"and",["dateUpdated","<","2017-04-25T16:00:00.000Z"]],"or",[["dateUpdated",">=","2017-04-30T16:00:00.000Z"],"and",["dateUpdated","<","2017-05-31T16:00:00.000Z"]]]]'
 ]
+let datakeys = ['dateUpdated', 'dateCreated', 'dateExported']
 testDemo.forEach((demo, index) => {
-    console.log("Demo " + (index + 1) + ':' + JSON.stringify(getAggregatefromFilter(demo), null, '    '))
+    console.log("Demo " + (index + 1) + ':' + JSON.stringify(parseFilter(demo,...datakeys), null, '    '))
 })
